4fb82baf7d29d9cdcc60f8b1a394f6f1d046c36d
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2018-05-22 00:27:27 -0300

    Publish and subscriber

---
+++ PublishAndSubscriber/main.js
@@ -0,0 +1,39 @@
+const channels = () => {
+    const channels = {}
+
+    const subscriber = (name, callback) => {
+        const backs = channels[name] || []
+
+        backs.push(callback)
+        channels[name] = backs
+    }
+
+    const publish = (name, message) => {
+        if (channels[name]) {
+            channels[name].forEach(element => {
+                console.log(name + ' ' + message)
+            });
+        }
+    }
+
+    return {
+        publish,
+        subscriber
+    }
+}
+
+
+
+const socket = channels()
+
+socket.subscriber('service_01', msg => {
+    console.log('service_01 called ...' + msg)
+})
+
+socket.subscriber('service_02', msg => {
+    console.log('service_02 called ...' + msg)
+})
+
+setInterval((id = Math.random()) => {
+    socket.publish('service_01', 'new message ' + id.toString(36).substring(2))
+}, 1000)
